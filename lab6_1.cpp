825115f0150c612145e364c1ab63735d0b211307
= <=>
2022-12-09 15:56:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int NumUser;
+    int Even;
+    int Odd;
+    
+    Even = 0; 
+    Odd = 0;
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> NumUser ;
+    
+    while( NumUser != 0 ){
+        
+        if(NumUser%2==0){
+            Even = Even+1;
+        }
+        else{
+            Odd = Odd+1;
+        }
+        cout << "Enter an integer: ";
+        cin >> NumUser ;
+    }
+    
+    cout << "#Even numbers = " << Even << "\n" ;
+    cout << "#Odd numbers = " << Odd << "\n"  ;
     return 0;
 }
+
